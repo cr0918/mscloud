396603ef88e5b8a9aecc4bf328fc9901ed93f5d7
chengrong <2877250948@qq.com>
2021-07-21 08:43:06 +0800

    springcloud provider

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,46 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@Api(description = "支付接口")
+public class PaymentController
+{
+    @Resource
+    private PaymentService paymentService;
+    @ApiOperation("创建支付")
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment)
+    {
+        int result = paymentService.create(payment);
+        log.info("*****插入操作返回结果:" + result);
+
+        if(result > 0)
+        {
+            return new CommonResult(200,"插入数据库成功",result);
+        }else{
+            return new CommonResult(444,"插入数据库失败",null);
+        }
+    }
+    @ApiOperation("创建支付流水信息")
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id)
+    {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*****查询结果:{}",payment);
+        if (payment != null) {
+            return new CommonResult(200,"查询成功",payment);
+        }else{
+            return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
+        }
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud.dao;
+
+import com.atguigu.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper  //import org.apache.ibatis.annotations.Mapper;mybatis提供的，等价：@MapperScan("com.atguigu.springcloud.dao")
+public interface PaymentDao
+{
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
+
+
