6c3c45c26bd3f59caf163a10df1799ba9d2eccbf
chengrong <2877250948@qq.com>
2021-07-21 18:17:18 +0800

    springcloud provider

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/MainApp80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/MainApp80.java
@@ -1,7 +1,9 @@
 package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class MainApp80 {
     public static void main(String[] args)
     {
